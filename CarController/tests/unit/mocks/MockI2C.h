8c1aa43f4208bf144326498ecdf838813ad5c9e5
masoares <manueljorgecruz@hotmail.com>
2025-02-27 10:30:18 +0000

    Refactor I2C test cases and mock classes for improved consistency and readability

--- CarController/tests/unit/mocks/MockI2C.h
+++ CarController/tests/unit/mocks/MockI2C.h
@@ -2,8 +2,9 @@
 #include <gmock/gmock.h>
 #include "I2C.hpp"
 
-class MockI2C : public I2C {
-public:
+class MockI2C : public I2C
+{
+  public:
     MOCK_METHOD(void, init, (const std::string&), (override));
     MOCK_METHOD(void, writeByte, (uint8_t, uint8_t, uint8_t), (override));
     MOCK_METHOD(uint8_t, readByte, (uint8_t), (override));
