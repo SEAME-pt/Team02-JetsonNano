8abe990f41b2409c3b603847474198c8badda981
Rui Pires <ruipedro.pires999@gmail.com>
2025-03-19 10:10:18 +0000

    Add zenoh::PosixShmProvider to LaneDetectorPublisher and ControllerPublisher for optimized memory allocation

--- src/Communication/LaneDetectorPublisher.cpp
+++ src/Communication/LaneDetectorPublisher.cpp
@@ -2,9 +2,9 @@
 
 LaneDetectorPublisher::LaneDetectorPublisher(
     std::shared_ptr<zenoh::Session> session)
+    : session_(session),
+      provider_(zenoh::MemoryLayout(65536, zenoh::AllocAlignment({2})))
 {
-    session_ = session;
-
     cameraError_pub.emplace(session_->declare_publisher(
         zenoh::KeyExpr("Vehicle/1/LaneDetection/CameraError")));
 }
@@ -12,5 +12,11 @@ LaneDetectorPublisher::LaneDetectorPublisher(
 void LaneDetectorPublisher::publishCameraError(float error)
 {
     std::cout << "Error send: " << error << std::endl;
-    cameraError_pub->put(std::to_string(error));
+    string value_str = to_string(error);
+    const auto len   = value_str.size() + 1;
+    auto alloc_result =
+        provider.alloc_gc_defrag_blocking(len, zenoh::AllocAlignment({0}));
+    zenoh::ZShmMut&& buf = std::get<zenoh::ZShmMut>(std::move(alloc_result));
+    memcpy(buf.data(), value_str.c_str(), len);
+    cameraError_pub->put(std::move(buf));
 }
