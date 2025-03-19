c470d7aedbef4b3719aec1e0c5995fc7bf618663
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-19 14:08:26 +0000

    Set integral gain in PID controller to zero for improved control stability

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
             pidController = new PidController();
         }
         float kp = 250;
-        float ki = 0.01;
+        float ki = 0.0;
         float kd = 2;
         float constant_throttle = 0.25;
         float delta_time = 0.05; //ms
