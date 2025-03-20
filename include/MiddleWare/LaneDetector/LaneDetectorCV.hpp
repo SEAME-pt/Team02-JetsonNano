6ad41733f60bcf7a22ecc3fc9dfbdb8224080a6d
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-20 11:34:52 +0000

    Reduce historySize from 10 to 5 for improved lane history management

--- include/MiddleWare/LaneDetector/LaneDetectorCV.hpp
+++ include/MiddleWare/LaneDetector/LaneDetectorCV.hpp
@@ -41,7 +41,7 @@ private:
 
     std::deque<std::vector<cv::Point>> leftLaneHistory;
     std::deque<std::vector<cv::Point>> rightLaneHistory;
-    const size_t historySize = 10; 
+    const size_t historySize = 5; 
     
 public:
     LaneDetectorCV(const std::string& pipeline, std::shared_ptr<zenoh::Session> session);
