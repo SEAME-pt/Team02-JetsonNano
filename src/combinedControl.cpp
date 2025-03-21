296cbd1aca94bfc9784e27292e5a0b97fd49a5ae
Luis Carvalho <filip.carvalho12@gmail.com>
2025-03-21 15:09:00 +0000

    updating ki value

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
             pidController = new PidController();
         }
         float kp = 210;
-        float ki = 0.000003;
+        float ki = 0.000001;
         float kd = 10;
         float constant_throttle = 0.35;
         float delta_time = 0.05; //ms
