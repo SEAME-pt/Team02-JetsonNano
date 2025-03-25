79d371a155b3d1b94c7a4764004773513ece2312
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-25 12:04:51 +0000

    fix: add debug output for CAN bus buffer in Signals::run

--- src/MiddleWare/Signals/Signals.cpp
+++ src/MiddleWare/Signals/Signals.cpp
@@ -20,6 +20,7 @@ void Signals::run()
     {
         usleep(300);
         int buffer = this->canBus->checktheReceive();
+        std::cout << buffer << std::endl;
         if (buffer != -1)
         {
             uint32_t can_id = 0;
