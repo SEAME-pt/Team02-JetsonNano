221f9889e3221c2403a4300e1e660b938a098e3a
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-27 12:00:54 +0000

    refactor: uncomment lane point visualization and comment out lane drawing for debugging

--- src/MiddleWare/LaneDetector/LaneDetector.cpp
+++ src/MiddleWare/LaneDetector/LaneDetector.cpp
@@ -330,9 +330,9 @@ void LaneDetector::postProcess(cv::Mat& frame)
 void LaneDetector::createLanes(std::vector<cv::Point> lanePoints, cv::Mat& frame)
 {
     // Initialize lane width estimate on first frame.
-    // for (const auto& pt : lanePoints) {
-    //     cv::circle(frame, pt, 2, cv::Scalar(255, 255, 255), -1); // White for all points
-    // }
+    for (const auto& pt : lanePoints) {
+        cv::circle(frame, pt, 2, cv::Scalar(255, 255, 255), -1); // White for all points
+    }
 
     if (firstFrame)
     {
@@ -553,7 +553,7 @@ void LaneDetector::createLanes(std::vector<cv::Point> lanePoints, cv::Mat& frame
 
 
     // 4. Draw the detected lanes
-    drawLanes(frame, leftCurve, rightCurve);
+    // drawLanes(frame, leftCurve, rightCurve);
 
 }
 
