a3644a47f13d10d8a2b92b3eecbed7670f36d1af
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-28 15:42:50 +0000

    refactor: update lane width frame skip and PID controller gain for improved performance

--- src/MiddleWare/LaneDetector/LaneDetector.cpp
+++ src/MiddleWare/LaneDetector/LaneDetector.cpp
@@ -11,7 +11,7 @@ Logger logger;
 
 LaneDetector::LaneDetector(const std::string& enginePath, const std::string& pipeline,
     std::shared_ptr<zenoh::Session> session)
-    : cap(pipeline, cv::CAP_GSTREAMER), session_(session), FRAME_SKIP(3), laneWidthEstimate(0.0), firstFrame(true),
+    : cap(pipeline, cv::CAP_GSTREAMER), session_(session), FRAME_SKIP(4), laneWidthEstimate(0.0), firstFrame(true),
     frame_count(0)
 {
     createExecutionContext(enginePath);
--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
             pidController    = new PidController();
         }
         // PID controller values
-        float kp                = 200;
+        float kp                = 250;
         float ki                = 0.000001;
         float kd                = 10;
         float constant_throttle = 0.3;
