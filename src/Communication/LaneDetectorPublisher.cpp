e2e9170907cf6cb44b7736c49d5740409b2443af
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-31 15:19:47 +0000

    refactor: remove unused metadata and options in publishCameraFrame method

--- src/Communication/LaneDetectorPublisher.cpp
+++ src/Communication/LaneDetectorPublisher.cpp
@@ -55,18 +55,18 @@ void LaneDetectorPublisher::publishCameraFrame(cv::Mat frame)
     
     
     // Create timestamp and metadata
-    frame_count_++;
-    std::string metadata = "frame_" + std::to_string(frame_count_) + 
-                           "_time_" + std::to_string(current_time);
+    // frame_count_++;
+    // std::string metadata = "frame_" + std::to_string(frame_count_) + 
+    //                        "_time_" + std::to_string(current_time);
     
      
     // Create Zenoh payload with metadata prefix
-    auto options = zenoh::Publisher::PutOptions::create_default();
-    options.encoding = zenoh::Encoding::Predefined::image_jpeg();
+    // auto options = zenoh::Publisher::PutOptions::create_default();
+    // options.encoding = zenoh::Encoding::Predefined::image_jpeg();
     //std::cout << payload << std::endl;
 
 
-    cameraFrame_pub->put(buffer, options);
+    cameraFrame_pub->put(buffer);
 }
 
 void LaneDetectorPublisher::publishLanes(
