2b8f8f5ceb68e5a1889dd298c89ff99a3c59884b
mjorgecruz <manueljorgecruz@hotmail.com>
2025-03-31 14:23:56 +0000

    refactor: remove debug output from publishCameraFrame method

--- src/Communication/LaneDetectorPublisher.cpp
+++ src/Communication/LaneDetectorPublisher.cpp
@@ -62,7 +62,7 @@ void LaneDetectorPublisher::publishCameraFrame(cv::Mat frame)
     std::vector<uint8_t> payload(metadata.begin(), metadata.end());
     payload.push_back(':'); // Separator
     payload.insert(payload.end(), buffer.begin(), buffer.end());
-    std::cout << payload << std::endl;
+    //std::cout << payload << std::endl;
     cameraFrame_pub->put(payload);
 }
 
