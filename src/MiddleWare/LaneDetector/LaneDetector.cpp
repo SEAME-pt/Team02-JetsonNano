82a690563349bba659a6e7f16ce500be743346bc
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-02 17:39:06 +0100

    refactor: comment out imshow call in LaneDetector run method

--- src/MiddleWare/LaneDetector/LaneDetector.cpp
+++ src/MiddleWare/LaneDetector/LaneDetector.cpp
@@ -234,7 +234,7 @@ void LaneDetector::run()
             remap(frame, undistorted, map1, map2, INTER_LINEAR);
             frame = undistorted;
             detect(frame);
-            imshow("Lane Detection", frame);
+            //imshow("Lane Detection", frame);
             publisher_->publishCameraFrame(frame);
 
         }
