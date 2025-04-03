e5a0bc3fa7531b84bd2410cbb34f74914129343b
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-03 11:13:13 +0100

    refactor: add debug output for angle and pulse width in ServoController setAngle method

--- src/Hardware/ServoController.cpp
+++ src/Hardware/ServoController.cpp
@@ -30,5 +30,6 @@ void ServoController::setAngle(int16_t angle)
     }
 
     uint16_t pulseWidth = static_cast<uint16_t>(150 + (angle * (260)) / 180);
+    std::cout << "angle: " << angle << "pw: " << pulseWidth << std::endl;
     m_ServoPCA->setPWM(0, 0, pulseWidth);
 }
