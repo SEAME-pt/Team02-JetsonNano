14e3fcedb794437ceaf3de14b57d29cf5a421296
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-03 11:06:19 +0100

    refactor: adjust pulse width calculation in ServoController setAngle method

--- src/Hardware/ServoController.cpp
+++ src/Hardware/ServoController.cpp
@@ -29,6 +29,6 @@ void ServoController::setAngle(int16_t angle)
         m_currAngle = angle;
     }
 
-    uint16_t pulseWidth = static_cast<uint16_t>(180 + (angle * (230)) / 180);
+    uint16_t pulseWidth = static_cast<uint16_t>(170 + (angle * (240)) / 180);
     m_ServoPCA->setPWM(0, 0, pulseWidth);
 }
