1bb65ae1a521abf789d504d7be3234a0b2070506
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-09 11:48:14 +0100

    fix: update camera properties in LaneDetector constructor and main pipeline configuration

--- src/laneDetector.cpp
+++ src/laneDetector.cpp
@@ -7,7 +7,8 @@ using namespace zenoh;
 int main(int argc, char** argv)
 {
     const std::string pipeline =
-        "nvarguscamerasrc sensor-id=0 ! "
+        "nvarguscamerasrc"
+        'sensor-id=1 ! "
         "video/x-raw(memory:NVMM), width=216, height=128, format=NV12, "
         "framerate=30/1 ! "
         "nvvidconv ! video/x-raw, format=BGRx ! "
