9e5fc6607d216881f9adec256eddaad902d15b5e
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-09 11:58:09 +0100

    fix: correct pipeline string formatting by adjusting sensor-id placement

--- src/laneDetector.cpp
+++ src/laneDetector.cpp
@@ -8,8 +8,8 @@ int main(int argc, char** argv)
 {
     const std::string pipeline =
         "nvarguscamerasrc"
-        "sensor-id=1 ! "
-        "video/x-raw(memory:NVMM), width=216, height=128, format=NV12, "
+        "sensor-id=1"
+        "! video/x-raw(memory:NVMM), width=216, height=128, format=NV12, "
         "framerate=30/1 ! "
         "nvvidconv ! video/x-raw, format=BGRx ! "
         "videoconvert ! video/x-raw, format=BGR ! "
