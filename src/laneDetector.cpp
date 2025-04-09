6f64d0dd795971348ce490da4e973cc69d70ceaa
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-09 13:59:18 +0100

    fix: correct formatting of GStreamer pipeline string for consistency

--- src/laneDetector.cpp
+++ src/laneDetector.cpp
@@ -7,11 +7,7 @@ using namespace zenoh;
 int main(int argc, char** argv)
 {
     const std::string pipeline =
-        "nvarguscamerasrc sensor-id=0 !"
-        "video/x-raw(memory:NVMM), width=(int)216, height=(int)128, format=NV12, framerate=(fraction)30/1 !"
-        "nvvidconv ! video/x-raw, format=BGRx !"
-        "videoconvert ! video/x-raw, format=BGR ! "
-        "appsink";
+        "nvarguscamerasrc ! sensor-id=0 ! video/x-raw(memory:NVMM), width=216, height=128, format=NV12, framerate= 30/1 ! nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
 
     try
     {
