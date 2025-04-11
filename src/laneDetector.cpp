438846e3ff90474c44c59c48b9b99fa7d5dea337
mjorgecruz <manueljorgecruz@hotmail.com>
2025-04-11 10:31:11 +0100

    fix: update video pipeline dimensions for lane detection

--- src/laneDetector.cpp
+++ src/laneDetector.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 {
     const std::string pipeline =
       "nvarguscamerasrc sensor-id=0 ! "
-      "video/x-raw(memory:NVMM), width=(int)320, height=(int)215, format=NV12, framerate=(fraction)30/1 ! "
+      "video/x-raw(memory:NVMM), width=(int)800, height=(int)600, format=NV12, framerate=(fraction)30/1 ! "
       "nvvidconv ! video/x-raw, format=BGRx ! "
       "videoconvert ! video/x-raw, format=BGR ! "
       "appsink";
