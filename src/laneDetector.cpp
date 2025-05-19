4be5b92ca52d2f18c681bb6dae1aa830a832aa44
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-19 13:02:35 +0100

    fix: optimize mask data access in postProcess function

--- src/laneDetector.cpp
+++ src/laneDetector.cpp
@@ -5,10 +5,10 @@ using namespace cv;
 using namespace std;
 using namespace zenoh;
 
-setenv("DISPLAY", ":0", 1);
-
 int main(int argc, char** argv)
 {
+    setenv("DISPLAY", ":0", 1);
+
     const std::string pipeline =
         "nvarguscamerasrc sensor-id=0 ! "
         "video/x-raw(memory:NVMM), width=(int)640, height=(int)480, "
