f6aeb2edbc352103de474d16300f99e1b1af2958
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-20 15:06:18 +0100

    refactor: remove unused cameraLanes_pub and related publishLanes method in LaneDetectorPublisher

--- src/Communication/LaneDetectorPublisher.cpp
+++ src/Communication/LaneDetectorPublisher.cpp
@@ -16,8 +16,6 @@ LaneDetectorPublisher::LaneDetectorPublisher(
 
     cameraError_pub.emplace(session_->declare_publisher(
         zenoh::KeyExpr("Vehicle/1/LaneDetection/CameraError")));
-    cameraLanes_pub.emplace(
-        session_->declare_publisher(zenoh::KeyExpr("Vehicle/1/LaneData")));
     cameraFrame_pub.emplace(
         session_->declare_publisher(zenoh::KeyExpr("video/stream")));
 }
@@ -55,39 +53,4 @@ void LaneDetectorPublisher::publishCameraFrame(cv::Mat frame)
 
     std::cout << "frame ready" << std::endl;
     cameraFrame_pub->put(buffer);
-}
-
-void LaneDetectorPublisher::publishLanes(const cv::Mat& leftLane,
-                                         const cv::Mat& rightLane)
-{
-    // Extract coefficients
-    float leftA =
-        static_cast<float>(leftCoeffs.at<double>(0)); // quadratic coefficient
-    float leftB =
-        static_cast<float>(leftCoeffs.at<double>(1)); // linear coefficient
-    float leftC = static_cast<float>(leftCoeffs.at<double>(2)); // constant term
-    float rightA =
-        static_cast<float>(rightCoeffs.at<double>(0)); // quadratic coefficient
-    float rightB =
-        static_cast<float>(rightCoeffs.at<double>(1)); // linear coefficient
-    float rightC =
-        static_cast<float>(rightCoeffs.at<double>(2)); // constant term
-
-    // Compute midline coefficients (simple average of left and right)
-    float midA = (leftA + rightA) / 2.0f;
-    float midB = (leftB + rightB) / 2.0f;
-    float midC = (leftC + rightC) / 2.0f;
-    std::cout << "Midline polynomial: " << midA << "y² + " << midB << "y + "
-              << midC << std::endl;
-
-    ss << midA << " " << midB << " " << midC;
-    std::string laneData = ss.str();
-
-    // Publish lane data
-    const auto len = laneData.size() + 1;
-    auto alloc_result =
-        provider_->alloc_gc_defrag_blocking(len, zenoh::AllocAlignment({0}));
-    zenoh::ZShmMut&& buf = std::get<zenoh::ZShmMut>(std::move(alloc_result));
-    memcpy(buf.data(), laneData.c_str(), len);
-    cameraLanes_pub->put(std::move(buf));
 }
