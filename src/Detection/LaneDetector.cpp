33fef6a220160dbef94a137fc1c96a2e0d663524
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-20 14:15:30 +0100

    fix: update cameraPitch to 25 degrees and horizontalFOV to 105 degrees for improved lane detection in LaneDetector

--- src/Detection/LaneDetector.cpp
+++ src/Detection/LaneDetector.cpp
@@ -47,15 +47,15 @@ LaneDetector::LaneDetector(const std::string& enginePath, std::shared_ptr<zenoh:
     // Calibrate IPM
 
     float cameraHeight = 0.13f;       // meters
-    float cameraPitch = 20.0f;       // degrees down from horizontal
-    float horizontalFOV = 95.0f;     // degrees
+    float cameraPitch = 25.0f;       // degrees down from horizontal
+    float horizontalFOV = 105.0f;     // degrees
     float img_height = static_cast<float>(HEIGHT);
     float img_width = static_cast<float>(WIDTH);
     float h_fov_rad = horizontalFOV * CV_PI / 180.0f;
     float verticalFOV = 2.0f * std::atan((img_height/img_width) * std::tan(h_fov_rad/2.0f)) * 180.0f / CV_PI;
     float nearDistance = 0.2f;       // meters
     float farDistance = 2.0f;       // meters
-    float laneWidth = 1.7f;          // meters
+    float laneWidth = 1.5f;          // meters
     bevSize = cv::Size(WIDTH, WIDTH);
     cv::Size origSize = cv::Size(WIDTH, HEIGHT);
     ipm.initialize(origSize, bevSize);
