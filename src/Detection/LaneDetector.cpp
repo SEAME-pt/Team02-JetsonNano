c5b67fb361f8911f823fbaee247136ad38271a6f
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-21 15:11:21 +0100

    fix: comment out staleness penalty calculations in lane distance adjustments

--- src/Detection/LaneDetector.cpp
+++ src/Detection/LaneDetector.cpp
@@ -340,17 +340,6 @@ void LaneDetector::createLanes(cv::Mat& binary_mask, cv::Mat& frame)
                                 calculateLaneDistance(lanePolylines[0], prevRightCurve) : 
                                 FLT_MAX;
             
-            // Apply staleness penalty only to valid memories
-            // if (hasValidLeftMemory) {
-            //     float leftStaleness = 1.0f + 0.05f * (currentFrame - leftLaneLastUpdatedFrame);
-            //     leftDistance *= leftStaleness;
-            // }
-            
-            // if (hasValidRightMemory) {
-            //     float rightStaleness = 1.0f + 0.05f * (currentFrame - rightLaneLastUpdatedFrame);
-            //     rightDistance *= rightStaleness;
-            // }
-            
             // Lower (adjusted) distance means better match
             isLeftLane = leftDistance < rightDistance;
             
