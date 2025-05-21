9f07230f37c59be4a84f5913e695a099f60ad8df
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-21 15:24:16 +0100

    fix: increase maximum lane memory frames from 10 to 15

--- include/Detection/LaneDetector.hpp
+++ include/Detection/LaneDetector.hpp
@@ -51,7 +51,7 @@ class LaneDetector
     int leftLaneLastUpdatedFrame = 0;
     int rightLaneLastUpdatedFrame = 0;
     int currentFrame = 0;
-    const int MAX_LANE_MEMORY_FRAMES = 10;
+    const int MAX_LANE_MEMORY_FRAMES = 15;
   public:
     std::shared_ptr<LaneDetectorPublisher> publisher_;
 
