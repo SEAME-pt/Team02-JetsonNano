91cbacd54a05ddabc7af4ca4f742ead25e11dc51
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-21 10:54:23 +0100

    fix: increase maxVerticalGap to enhance lane merging accuracy

--- src/Detection/LaneDetector.cpp
+++ src/Detection/LaneDetector.cpp
@@ -210,7 +210,7 @@ void LaneDetector::createLanes(cv::Mat& binary_mask, cv::Mat& frame)
     std::vector<std::vector<cv::Point>> lanePolylines = processLaneMask(binary_mask, 30, 40, 6);
     
     float maxHorizontalDistance = frame.cols * 0.1; // 10% of frame width
-    float maxVerticalGap = frame.rows * 0.2;       // 20% of frame height
+    float maxVerticalGap = frame.rows * 0.3;       // 20% of frame height
     mergeLaneComponents(lanePolylines, maxHorizontalDistance, maxVerticalGap);
 
     cv::Mat allPolylinesViz = frame.clone();
