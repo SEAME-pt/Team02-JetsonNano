5c563788787508d0c60721a0f6adb2dcb50a6230
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-22 12:13:56 +0100

    fix: update speed PID parameters for improved control response

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -159,7 +159,7 @@ PidController::PidController(const std::string& configFile,
     current_speed_    = 0.0f;
     speed_lock_       = false;
     speedPidController_ = new SpeedPidController();
-    speedPidController_->init(1.0f, 0.0f, 1.0f,
+    speedPidController_->init(3.0f, 0.005f, 1.0f,
                                fixed_delta_time_);
 
     auto config = zenoh::Config::from_file(configFile);
@@ -390,7 +390,7 @@ void PidController::run()
         {
             std::cout << "Speed :" << current_speed_ << std::endl;
             updateControl(cameraError_, current_time);
-            publisher_->publishSpeed(speedPidController_->speedPID(20 - current_speed_, current_time));
+            publisher_->publishSpeed(speedPidController_->speedPID(5 - current_speed_, current_time));
             std::this_thread::sleep_for(std::chrono::milliseconds(
                 static_cast<int>(fixed_delta_time_ * 1000)));
         }
