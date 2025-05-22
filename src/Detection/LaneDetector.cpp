b590e3fa912354e453bf3ae83fa7cea126f2e195
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-22 11:53:34 +0100

    feat: initialize Kalman filters in LaneDetector constructor

--- src/Detection/LaneDetector.cpp
+++ src/Detection/LaneDetector.cpp
@@ -62,6 +62,8 @@ LaneDetector::LaneDetector(const std::string& enginePath, std::shared_ptr<zenoh:
     ipm.calibrateFromCamera(cameraHeight, cameraPitch, horizontalFOV, verticalFOV,
                             nearDistance, farDistance, laneWidth);
 
+    initializeKalmanFilters();
+
     publisher_ = std::make_shared<LaneDetectorPublisher>(session_);
 
     try     
