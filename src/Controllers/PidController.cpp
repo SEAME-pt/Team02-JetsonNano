b4b783c23fa1540f76d16fe9f9c415ed11330153
mjorgecruz <manueljorgecruz@hotmail.com>
2025-05-26 12:45:04 +0100

    fix: update speed PID controller initialization parameters for improved performance

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -46,7 +46,7 @@ PidController::PidController(XboxController* xbox_controller)
     current_speed_    = 0.0f;
     speed_lock_       = false;
     speedPidController_ = new SpeedPidController();
-    speedPidController_->init(0.1f, 0.05f, 0.005f,
+    speedPidController_->init(0.1f, 0.15f, 0.05f,
                                fixed_delta_time_);
 
     auto config = zenoh::Config::create_default();
