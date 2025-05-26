24a5419efc5dd7dbe076a6e7ef9952bbdfa5cb41
mjorgecruz <manueljorgecruz@hotmail.com>
2025-05-26 10:20:13 +0100

    fix: comment out speed calculation and publishing logic in Signals::run for debugging purposes

--- src/MiddleWare/Signals/Signals.cpp
+++ src/MiddleWare/Signals/Signals.cpp
@@ -34,10 +34,10 @@ void Signals::run()
                 memcpy(&speed, &data[1], 4);
 
                 speed = ntohl(speed);
-                speed = wheelDiame * 3.14 * speed * 10 / 60;
-                if (speed < 0 || speed > 100)
-                    speed = 0;
-                printf("Publishing speed: '%d'\n", speed);
+                // speed = wheelDiame * 3.14 * speed * 10 / 60;
+                // if (speed < 0 || speed > 100)
+                //     speed = 0;
+                // printf("Publishing speed: '%d'\n", speed);
                 std::string speed_str = std::to_string(speed);
                 publisher_->publishSpeed(std::stof(speed_str));
             }
