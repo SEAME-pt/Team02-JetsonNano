03f1566c94990d01c4059fab209058b2ccb13a22
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-26 14:30:20 +0100

    fix: update speed calculation in PidController run method for improved responsiveness

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -390,7 +390,7 @@ void PidController::run()
         {
             std::cout << "Speed :" << current_speed_ << std::endl;
             updateControl(cameraError_, current_time);
-            publisher_->publishSpeed(speedPidController_->speedPID(50 - current_speed_, current_time));
+            publisher_->publishSpeed(speedPidController_->speedPID(100 - current_speed_, current_time));
             std::this_thread::sleep_for(std::chrono::milliseconds(
                 static_cast<int>(fixed_delta_time_ * 1000)));
         }
