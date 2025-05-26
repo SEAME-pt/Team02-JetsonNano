acef2a5bf531750c77409d65691cdfeb8a842def
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-05-26 14:24:14 +0100

    fix: adjust speed calculation in PidController run method for improved control

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -390,7 +390,7 @@ void PidController::run()
         {
             std::cout << "Speed :" << current_speed_ << std::endl;
             updateControl(cameraError_, current_time);
-            publisher_->publishSpeed(speedPidController_->speedPID(180 - current_speed_, current_time));
+            publisher_->publishSpeed(speedPidController_->speedPID(30 - current_speed_, current_time));
             std::this_thread::sleep_for(std::chrono::milliseconds(
                 static_cast<int>(fixed_delta_time_ * 1000)));
         }
