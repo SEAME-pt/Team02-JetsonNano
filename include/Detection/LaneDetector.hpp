7b794aeb7d5a864bb538d3f295a1c740f5bc29ae
ruipires <ruipedro.pires999@gmail.com>
2025-05-27 10:41:49 +0100

    feat: add frame width and height attributes for improved lane detection calculations

--- include/Detection/LaneDetector.hpp
+++ include/Detection/LaneDetector.hpp
@@ -50,6 +50,8 @@ class LaneDetector
     std::vector<cv::Point> prevRightCurve;
 
     cv::Mat allPolylinesViz_;
+    int frameWidth_;
+    int frameHeight_;
 
     int leftLaneLastUpdatedFrame = 0;
     int rightLaneLastUpdatedFrame = 0;
@@ -72,13 +74,13 @@ class LaneDetector
 
     void createLanes(cv::Mat& binary_mask, cv::Mat& frame);
     std::vector<std::vector<cv::Point>> clusterLaneMask(const cv::Mat& laneMask, int kernelSize, int minArea, int maxLanes);
-    void mergeLaneComponents(std::vector<std::vector<cv::Point>>& lanePolylines, float maxHorizontalDist, float maxVerticalGap, int width);
+    void mergeLaneComponents(std::vector<std::vector<cv::Point>>& lanePolylines, float maxHorizontalDist, float maxVerticalGap);
     void drawPolyLanes(std::vector<std::vector<cv::Point>> lanePolylines);
     bool checkIfLeftLane(const std::vector<std::vector<cv::Point>> &lanePolylines);
 
     float calculateLaneDistance(const std::vector<cv::Point>& lane1, const std::vector<cv::Point>& lane2);
     bool validateLaneSeparation(const std::vector<std::vector<cv::Point>>& lanePolylines, float minLaneWidth);
-    void checkPredicedCurve(std::vector<cv::Point>& predictedCurve, const std::vector<cv::Point>& realLane, bool isLeftLane, int width);
+    void checkPredicedCurve(std::vector<cv::Point>& predictedCurve, const std::vector<cv::Point>& realLane, bool isLeftLane);
     void defineTrajectoryCurve(std::vector<cv::Point>& midCurve, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve);
     void createMidPointError(std::vector<cv::Point>& midCurve, cv::Mat frame);
     
