273d85a179efc98a466a626bfb290dc096cb6ad6
ruipires <ruipedro.pires999@gmail.com>
2025-05-27 10:25:34 +0100

    refactor: simplify drawPolyLanes and curve prediction methods for improved clarity and maintainability

--- include/Detection/LaneDetector.hpp
+++ include/Detection/LaneDetector.hpp
@@ -49,6 +49,8 @@ class LaneDetector
     std::vector<cv::Point> prevLeftCurve;
     std::vector<cv::Point> prevRightCurve;
 
+    cv::Mat allPolylinesViz_;
+
     int leftLaneLastUpdatedFrame = 0;
     int rightLaneLastUpdatedFrame = 0;
     int currentFrame = 0;
@@ -71,13 +73,13 @@ class LaneDetector
     void createLanes(cv::Mat& binary_mask, cv::Mat& frame);
     std::vector<std::vector<cv::Point>> clusterLaneMask(const cv::Mat& laneMask, int kernelSize, int minArea, int maxLanes);
     void mergeLaneComponents(std::vector<std::vector<cv::Point>>& lanePolylines, float maxHorizontalDist, float maxVerticalGap, int width);
-    void drawPolyLanes(std::vector<std::vector<cv::Point>> lanePolylines, cv::Mat& allPolylinesViz);
+    void drawPolyLanes(std::vector<std::vector<cv::Point>> lanePolylines);
 
     float calculateLaneDistance(const std::vector<cv::Point>& lane1, const std::vector<cv::Point>& lane2);
     bool validateLaneSeparation(const std::vector<std::vector<cv::Point>>& lanePolylines, float minLaneWidth);
-    void checkPredicedCurve(std::vector<cv::Point>& predictedCurve, const std::vector<cv::Point>& realLane, cv::Mat& allPolylinesViz, bool isLeftLane, int width);
-    void defineTrajectoryCurve(std::vector<cv::Point>& midCurve, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve, cv::Mat& allPolylinesViz);
-    void createMidPointError(std::vector<cv::Point>& midCurve, cv::Mat frame, cv::Mat& allPolylinesViz);
+    void checkPredicedCurve(std::vector<cv::Point>& predictedCurve, const std::vector<cv::Point>& realLane, bool isLeftLane, int width);
+    void defineTrajectoryCurve(std::vector<cv::Point>& midCurve, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve);
+    void createMidPointError(std::vector<cv::Point>& midCurve, cv::Mat frame);
     
     void createExecutionContext(const std::string& enginePath);
 };
