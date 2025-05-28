2dad22b3d3bc4cba09d1288118c1860baaac6d2a
ruipires <ruipedro.pirees999@gmail.com>
2025-05-28 11:32:56 +0100

    fix: add obstacle detection warning message in checkForwardCollision method

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -727,6 +727,10 @@ void TrajectoryDefinition::checkForwardCollision(const cv::Mat& segmentation_mas
 
     if (danger_detected)
     {
+         cv::putText(allPolylinesViz_, "OBSTACLE DETECTED!",
+                    cv::Point(frame.cols / 2 - 150, frame.rows / 2),
+                    cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 0, 255), 3);
+
         std::cout << "\033[1;31m*** WARNING: OBSTACLE DETECTED! STOPPING "
                      "VEHICLE ***\033[0m"
                   << std::endl;
