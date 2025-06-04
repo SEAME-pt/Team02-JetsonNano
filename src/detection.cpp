da53af3b86513a5456a672f2644a44309ca2c289
ruipires <ruipedro.pirees999@gmail.com>
2025-06-04 21:32:11 +0100

    feat: add base64 encoding for image data and update trajectory frame publishing method

--- src/detection.cpp
+++ src/detection.cpp
@@ -177,6 +177,52 @@ void objectDetectionThreadFunction(ObjectDetector* detector,
     }
 }
 
+
+// Base64 encoding function
+static const std::string base64_chars = 
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
+
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    while (in_len--) {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for(i = 0; (i < 4); i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for(j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+        for (j = 0; (j < i + 1); j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while((i++ < 3))
+            ret += '=';
+    }
+
+    return ret;
+}
+
 void trajectoryThreadFunction(TrajectoryDefinition* trajectoryDef,
                               SynchronizedProcessor* processor)
 {   
@@ -188,7 +234,16 @@ void trajectoryThreadFunction(TrajectoryDefinition* trajectoryDef,
 
         cv::Mat new_frame = trajectoryDef->process(original_frame, lane_mask, object_mask);
 
-        trajectoryDef->frame_publisher_->put("hello");
+            // Encode new_frame as JPEG
+        std::vector<uchar> buffer;
+        std::vector<int> params = {cv::IMWRITE_JPEG_QUALITY, 85}; // Adjust quality as needed
+        cv::imencode(".jpg", new_frame, buffer, params);
+        
+        // Base64 encode the image data
+        std::string base64_data = base64_encode(buffer.data(), buffer.size());
+        
+        // Publish the encoded frame data
+        trajectoryDef->frame_publisher_->put(base64_data);
 
         processor->trajectoryDone();
     }
@@ -210,8 +265,8 @@ int main(int argc, char** argv)
         else
         {
             auto config = Config::create_default();
-            config.insert_json5("listen/endpoints", "[\"tcp/100.117.122.95:7450\"]");
-            config.insert_json5("connect/endpoints", "[\"tcp/100.117.122.95:7447\"]");
+            config.insert_json5("listen/endpoints", "[\"udp/100.117.122.95:7450\"]");
+            config.insert_json5("connect/endpoints", "[\"udp/100.117.122.95:7447\"]");
             session     = std::make_shared<zenoh::Session>(
                 zenoh::Session::open(std::move(config)));
         }
