052ac40d56807e55eae34cb42d6b771db203b597
mjorgecruz <manueljorgecruz@hotmail.com>
2025-06-04 16:05:06 +0100

    fix: update visualizeGrid method to set actualSize to fixed dimensions for consistent overlay resizing

--- src/Detection/ObstacleAvoidance.cpp
+++ src/Detection/ObstacleAvoidance.cpp
@@ -366,7 +366,8 @@ void ObstacleAvoidance::visualizeGrid(
             }
         }
     }
-    cv::Size actualSize = cv::Size(800, static_cast<int>(800.0 * 0.45 / 1.4));;
+    // cv::Size actualSize = cv::Size(800, static_cast<int>(800.0 * 0.45 / 1.4));
+    cv::Size actualSize = cv::Size(800, 600);
     cv::resize(overlay, overlay, actualSize);
     // cv::addWeighted(overlay, 1, visualImage, 0, 0, visualImage);
     
