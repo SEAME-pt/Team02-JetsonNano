f27fa7b2e9bd9fedbcd3e8c4af9a705970221786
ruipires <ruipedro.pirees999@gmail.com>
2025-06-05 15:12:30 +0100

    feat: add PID control toggle and integrate Model Predictive Controller in main execution flow

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -1,4 +1,5 @@
 #include "XboxController.hpp"
+#include "MPController.hpp"
 #include "PidController.hpp"
 
 int main(int argc, char** argv)
@@ -21,6 +22,7 @@ int main(int argc, char** argv)
 
         XboxController manualController(session);
         PidController pidController(session, &manualController);
+        ModelPredictiveController MPController(session, &manualController);
 
         // PID controller values
         // float kp                = 130;
@@ -32,14 +34,33 @@ int main(int argc, char** argv)
         float constant_throttle = 250;
         float delta_time        = 0.05; // ms
 
+        // MPC controller values
+        size_t N  = 10;  // steps
+        double L  = 2.9; // distance between axis
+        double Ts = 0.1; // time between control actions
+
+        Eigen::Matrix4d Q = Eigen::Matrix4d::Zero();
+        Q(0,0) = 100.0;  // x position error weight
+        Q(1,1) = 100.0;  // y position error weight
+        Q(2,2) = 10.0;   // heading error weight
+        Q(3,3) = 1.0;    // velocity error weight
+
+        // Control input weight matrix (R)
+        Eigen::Matrix2d R = Eigen::Matrix2d::Zero();
+        R(0,0) = 0.1;    // steering input weight
+        R(1,1) = 10.0;       
+        
+        Eigen::Matrix4d Qf = Q;
+
         pidController.init(kp, ki, kd, constant_throttle, delta_time);
+        MPController.init(N, L, Ts, Q, R, Qf);
 
         std::thread manualThread(&XboxController::run, &manualController);
         std::thread pidThread(&PidController::run, &pidController);
+        std::thread MPCThread(&ModelPredictiveController::run, &MPController);
         manualThread.join();
         pidThread.join();
-
-        session->close();
+        MPCThread.join();
     }
     catch (const std::exception& e)
     {
