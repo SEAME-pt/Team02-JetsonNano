5c8caad5f95b301706a44365f9bdf60da9e46eb4
ruipires <ruipedro.pirees999@gmail.com>
2025-06-05 13:38:23 +0100

    refactor: remove unused PID subscriber definitions and update control methods in PidController and SpeedPidController

--- include/Controllers/PidController.hpp
+++ include/Controllers/PidController.hpp
@@ -26,15 +26,11 @@ class PidController
 private:
     std::shared_ptr<zenoh::Session> session_;
     std::unique_ptr<ControllerPublisher> publisher_;
-    std::optional<zenoh::Subscriber<void>> kp_subscriber;
-    std::optional<zenoh::Subscriber<void>> ki_subscriber;
-    std::optional<zenoh::Subscriber<void>> kd_subscriber;
     std::optional<zenoh::Subscriber<void>> cameraError_subscriber;
     std::optional<zenoh::Subscriber<void>> activeAutonomyLevel_subscriber;
     std::optional<zenoh::Subscriber<void>> currentSpeed_subscriber;
     std::optional<zenoh::Subscriber<void>> speed_lock_subscriber;
     
-    // PID constants
     float kp_; // Proportional gain
     float ki_; // Integral gain
     float kd_; // Derivative gain
@@ -45,15 +41,13 @@ private:
     float speedKd_;
     float current_speed_;
 
-    // PID variables
     float prev_error_;
     float cameraError_;
     float integral_;
     double last_time_;
     
-    // Control parameters
-    float constant_speed_; // Constant speed for the car
-    float max_steering_angle_; // Maximum steering angle
+    float constant_speed_;
+    float max_steering_angle_;
 
     float fixed_delta_time_;
 
@@ -75,7 +69,9 @@ public:
     void init(float kp, float ki, float kd, float speed, float delta_time);
     
     float steeringPID(float error, double current_time);
-    float speedAdjustment(float error);
+
+    //SAE_0
+    void manualControl(float manual_steering, float manual_speed);
 
     //SAE_1
     void LKASControl(float lane_error, double current_time, float manual_steering, float manual_speed);
@@ -88,7 +84,7 @@ public:
     void conditionalAutomation(float lane_error, double current_time);
 
     //SAE_4
-    void updateControl(float lane_error, double current_time);
+    void autonomousControl(float lane_error, double current_time);
 
     void setAutonomousDriveState(std::string current_state);
     std::string getAutonomousDriveState() const;
