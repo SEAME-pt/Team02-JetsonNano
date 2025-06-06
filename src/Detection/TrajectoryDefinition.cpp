a8b142ee224fe5498b4e764f0af84b13ef48c3b6
mjorgecruz <manueljorgecruz@hotmail.com>
2025-06-06 13:58:35 +0100

    fix: comment out checkForwardCollision call in createLanes function to prevent collision checks

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -300,7 +300,7 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
     
     createMidPointError(midCurve);
     
-    checkForwardCollision(class_mask, midCurve);
+    // checkForwardCollision(class_mask, midCurve);
 
     allPolylinesViz_.copyTo(frame);
 }
