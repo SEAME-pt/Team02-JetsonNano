12a990b85b8ffaf15b5fbd9be65bacb13ab74a8d
ruipires <ruipedro.pires999@gmail.com>
2025-06-13 13:30:50 +0100

    fix: correct pulse width calculation in ServoController setAngle method

--- src/Hardware/ServoController.cpp
+++ src/Hardware/ServoController.cpp
@@ -30,7 +30,7 @@ void ServoController::setAngle(int16_t angle)
     }
 
     uint16_t pulseWidth =
-        static_cast<uint16_t>(308 + ((angle - 90) * 150 / 90));
+        static_cast<uint16_t>(302 + ((angle - 90) * 150 / 90));
     std::cout << "angle: " << angle << "pw: " << pulseWidth << std::endl;
     m_ServoPCA->setPWM(0, 0, pulseWidth);
 }
