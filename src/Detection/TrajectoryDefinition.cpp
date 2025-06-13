0b7b28f16c3d750748009779a7245213d3cc7399
ruipires <ruipedro.pires999@gmail.com>
2025-06-13 18:01:13 +0100

    fix: comment out drawPolyLanes call in createLanes method for debugging purposes

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -169,7 +169,7 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
 
     lanePolylines = clusterLaneMask(binary_mask, 30, 40, 6);
 
-    drawPolyLanes(lanePolylines);
+    // drawPolyLanes(lanePolylines);
     
     float maxHorizontalDistance = frameWidth_ * 0.05;  // 15% of frame width
     float maxVerticalGap        = frameHeight_ * 0.15; // 35% of frame height
