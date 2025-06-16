0dc86a44c6b05856d8c4907b7aaa5f7bbc64212f
masoares <mjsmarquescruz@gmail.com>
2025-06-16 15:01:07 +0100

    fix: reduce sleep duration in main loop for improved responsiveness

--- src/detection.cpp
+++ src/detection.cpp
@@ -199,7 +199,7 @@ int main(int argc, char** argv)
 
         std::cout << "Running. Press Ctrl+C to exit." << std::endl;
         while(running) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
         
         std::cout << "Shutting down..." << std::endl;
