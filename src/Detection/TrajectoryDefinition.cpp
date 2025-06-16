89ad683c342f5acbd886ddbdba9fe46f5a871359
masoares <mjsmarquescruz@gmail.com>
2025-06-16 14:30:56 +0100

    fix: adjust far distance and lane width parameters in initLocalEnv method

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -98,8 +98,8 @@ void TrajectoryDefinition::initLocalEnv() {
         float h_fov_rad = horizontalFOV * CV_PI / 180.0f;
         float verticalFOV = 2.0f * std::atan((img_height/img_width) * std::tan(h_fov_rad/2.0f)) * 180.0f / CV_PI;
         float nearDistance = 0.01f;       // meters
-        float farDistance = 0.45f;       // meters
-        float laneWidth = 1.4f;      // meters
+        float farDistance = 1.0f;       // meters
+        float laneWidth = 1.0f;      // meters
         cv::Size bevSize = cv::Size(800, 600);
         cv::Size origSize = cv::Size(800, 600);
 
@@ -189,7 +189,7 @@ cv::Mat TrajectoryDefinition::process(cv::Mat& frame, cv::Mat& binary_mask,
     createLanes(ipm_frame, ipm_binary_mask, ipm_class_mask);
 
     // cv::Size size(800 * 6.0 / 8.0, 600);
-    cv::Size size(800 * 1.4 / 0.45 , 600);
+    cv::Size size(800 * 1.0 / 1.0 , 600);
 
 
     cv::Mat res_frame;
@@ -253,7 +253,7 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
     
     drawCurves(midCurve, leftCurve, rightCurve);
     (void) class_mask;
-    obstacleAvoidance(class_mask, midCurve);
+    // obstacleAvoidance(class_mask, midCurve);
    
     createMidPointError(midCurve);
     
