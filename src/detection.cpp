d05e665b2dd31d18ebc4015df004d56fc8762ad7
ruipires <ruipedro.pires999@gmail.com>
2025-06-17 14:27:26 +0100

    fix: update lane detection model file path for local environment

--- src/detection.cpp
+++ src/detection.cpp
@@ -177,7 +177,7 @@ int main(int argc, char** argv)
                 "appsink";
             camera.initLocalEnv(pipeline, "calibration.yml");
             trajectoryDefinition.initLocalEnv();
-            laneDetectionFile = "/home/team02/Models/engine/lane_Yolo_local1_epoch_25.engine";
+            laneDetectionFile = "/home/team02/Models/engine/lane_Yolo_local1_epoch_50.engine";
             objDetectionFile = "/home/team02/Models/engine/obj_Yolo_local1_epoch_200.engine";
         } else {
             std::cout << "Running in CARLA mode with simulated camera" << std::endl;
