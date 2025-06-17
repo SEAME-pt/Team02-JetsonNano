3f438ee8bd9b5240c625b926d3beae6e85304cd8
ruipires <ruipedro.pirees999@gmail.com>
2025-06-17 11:19:25 +0100

    fix: update object detection model file path in main function

--- src/detection.cpp
+++ src/detection.cpp
@@ -178,7 +178,7 @@ int main(int argc, char** argv)
             camera.initLocalEnv(pipeline, "calibration.yml");
             trajectoryDefinition.initLocalEnv();
             laneDetectionFile = "/home/team02/Models/engine/lane_Yolo_local1_epoch_25.engine";
-            objDetectionFile = "/home/team02/Models/engine/obj_MOB_1_epoch_133.engine";
+            objDetectionFile = "/home/team02/Models/engine/obj_Yolo_local1_epoch_82.engine";
         } else {
             std::cout << "Running in CARLA mode with simulated camera" << std::endl;
             camera.initCarlaEnv();
