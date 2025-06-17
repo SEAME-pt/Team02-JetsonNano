21949a36cf2a4d87cac6fcbc65def12bb3a315b1
ruipires <ruipedro.pires999@gmail.com>
2025-06-17 14:53:00 +0100

    fix: comment out lane polyline drawing and enable curve drawing in createLanes function

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -217,7 +217,7 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
 
     lanePolylines = clusterLaneMask(binary_mask, 30, 40, 6);
 
-    drawPolyLanes(lanePolylines);
+    // drawPolyLanes(lanePolylines);
     
     float maxHorizontalDistance = frameWidth_ * 0.15;  // 15% of frame width
     float maxVerticalGap        = frameHeight_ * 0.20; // 20% of frame height
@@ -248,7 +248,7 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
 
     defineTrajectoryCurve(midCurve, leftCurve, rightCurve);
     
-    // drawCurves(midCurve, leftCurve, rightCurve);
+    drawCurves(midCurve, leftCurve, rightCurve);
     (void) class_mask;
     // obstacleAvoidance(class_mask, midCurve);
    
