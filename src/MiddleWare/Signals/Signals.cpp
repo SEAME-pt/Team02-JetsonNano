a7154cb400b364e604d418a2dddea823d3516287
masoares <mjsmarquescruz@gmail.com>
2025-06-25 15:56:44 +0100

    fix: adjust sleep duration in Signals::run() for improved responsiveness

--- src/MiddleWare/Signals/Signals.cpp
+++ src/MiddleWare/Signals/Signals.cpp
@@ -67,7 +67,7 @@ void Signals::run()
 {
     while (1)
     {
-        usleep(10);
+        usleep(25000);  
 
         if (this->canBus) {
             int buffer = this->canBus->checktheReceive();
