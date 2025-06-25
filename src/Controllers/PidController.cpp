d1a0efea66cf6b9a2fffe922d341a72b3eb34e26
masoares <mjsmarquescruz@gmail.com>
2025-06-25 12:04:49 +0100

    fix: update speedKp and speedKi parameters in PidController for improved speed control

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -28,10 +28,10 @@ PidController::PidController(std::shared_ptr<zenoh::Session> session, XboxContro
     speed_lock_         = false;
     xboxController_     = xbox_controller;
     current_speed_      = 0.0f;
-    speedKp_ = 0.1f;
+    speedKp_ = 0.2f;
     // speedKi_ = 1.3f;
     // speedKd_ = 0.01f;    
-    speedKi_ = 0.000f;
+    speedKi_ = 0.01f;
     speedKd_ = 0.0f;
     speedPidController_ = new SpeedPidController();
     speedPidController_->init(speedKp_, speedKi_, speedKd_, fixed_delta_time_);
