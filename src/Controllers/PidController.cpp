cf506cce3816e6e0c3eba8d431b525352aa36c74
masoares <mjsmarquescruz@gmail.com>
2025-06-25 12:08:11 +0100

    fix: set speedKd parameter to 0.01f in PidController for improved speed control

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -31,7 +31,7 @@ PidController::PidController(std::shared_ptr<zenoh::Session> session, XboxContro
     speedKp_ = 0.2f;
     // speedKi_ = 1.3f;
     // speedKd_ = 0.01f;    
-    speedKi_ = 0.01f;
+    speedKi_ = 0.1f;
     speedKd_ = 0.01f;
     speedPidController_ = new SpeedPidController();
     speedPidController_->init(speedKp_, speedKi_, speedKd_, fixed_delta_time_);
