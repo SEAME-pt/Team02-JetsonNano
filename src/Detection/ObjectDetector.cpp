496fc093246dd673954ad2b5932bdd1a4631b496
ruipires <ruipedro.pires999@gmail.com>
2025-06-26 10:49:03 +0100

    fix: change image resizing method from INTER_NEAREST to INTER_LINEAR for better quality

--- src/Detection/ObjectDetector.cpp
+++ src/Detection/ObjectDetector.cpp
@@ -41,7 +41,7 @@ void ObjectDetector::preProcess(cv::Mat& frame, cv::Mat& preprocessedFrame)
 {
     cv::Mat resized;
     
-    cv::resize(frame, resized, cv::Size(width_, height_), 0, 0, cv::INTER_NEAREST);
+    cv::resize(frame, resized, cv::Size(width_, height_), 0, 0, cv::INTER_LINEAR);
     
     cv::cvtColor(resized, preprocessedFrame, cv::COLOR_BGR2RGB);
 }
