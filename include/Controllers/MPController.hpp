806568fe63733eac03e8c49c872d52b064d2b060
masoares <mjsmarquescruz@gmail.com>
2025-06-27 08:39:52 +0100

    refactor: clean up ModelPredictiveController header file structure

--- include/Controllers/MPController.hpp
+++ include/Controllers/MPController.hpp
@@ -22,7 +22,6 @@ private:
     std::optional<zenoh::Subscriber<void>> coeffs_subscriber;
     std::optional<zenoh::Subscriber<void>> activeAutonomyLevel_subscriber;
     std::optional<zenoh::Subscriber<void>> speed_lock_subscriber;
-    std::optional<zenoh::Subscriber<void>> currentSpeed_subscriber;
 
     bool carlaMode_ = false;
 
@@ -32,10 +31,7 @@ private:
 
     float lane_departure_threshold_;
 
-    SpeedPidController* speedPidController_;
-    float speedKp_;
-    float speedKi_;
-    float speedKd_;
+
     float current_speed_;
     float desired_speed_;
     float current_steering_;
