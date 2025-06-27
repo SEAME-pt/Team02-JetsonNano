f3f9fa8cae57924573f13730658c9b68591377b3
ruipires <ruipedro.pirees999@gmail.com>
2025-06-27 13:24:16 +0100

    fix: enable publishing of IPM frame, original frame, lane mask, and object mask in trajectoryThreadFunction

--- src/detection.cpp
+++ src/detection.cpp
@@ -103,30 +103,22 @@ void trajectoryThreadFunction(TrajectoryDefinition* trajectoryDef,
             std::vector<uchar> buffer_ipm_frame;
             std::vector<int> params_ipm = {cv::IMWRITE_JPEG_QUALITY, 20};
             cv::imencode(".jpg", new_frame, buffer_ipm_frame, params_ipm);
-            
-            // trajectoryDef->publishIPMFrame(std::string(buffer_ipm_frame.begin(), buffer_ipm_frame.end()));
-            trajectoryDef->ipm_frame_publisher_->put(buffer_ipm_frame);
+            trajectoryDef->publishIPMFrame(std::string(buffer_ipm_frame.begin(), buffer_ipm_frame.end()));
     
             std::vector<uchar> buffer_original_frame;
             std::vector<int> params_org_frame = {cv::IMWRITE_JPEG_QUALITY, 20};
             cv::imencode(".jpg", original_frame, buffer_original_frame, params_org_frame);
-            trajectoryDef->frame_publisher_->put(buffer_original_frame);
-            
-            // trajectoryDef->publishOrigFrame(std::string(buffer_original_frame.begin(), buffer_original_frame.end()));
+            trajectoryDef->publishOrigFrame(std::string(buffer_original_frame.begin(), buffer_original_frame.end()));
     
             std::vector<uchar> buffer_lane_mask;
             std::vector<int> params_lane = {cv::IMWRITE_JPEG_QUALITY, 20};
             cv::imencode(".jpg", lane_mask, buffer_lane_mask, params_lane);
-            trajectoryDef->lane_mask_publisher_->put(buffer_lane_mask);
-            
-            // trajectoryDef->publishBinMask(std::string(buffer_lane_mask.begin(), buffer_lane_mask.end()));
+            trajectoryDef->publishBinMask(std::string(buffer_lane_mask.begin(), buffer_lane_mask.end()));
     
             std::vector<uchar> buffer_obj_mask;
             std::vector<int> params_obj = {cv::IMWRITE_JPEG_QUALITY, 20};
             cv::imencode(".jpg", object_mask, buffer_obj_mask, params_obj);
-            trajectoryDef->class_mask_publisher_->put(buffer_obj_mask);
-            
-            // trajectoryDef->publishClassMask(std::string(buffer_obj_mask.begin(), buffer_obj_mask.end()));
+            trajectoryDef->publishClassMask(std::string(buffer_obj_mask.begin(), buffer_obj_mask.end()));
     
             processor->trajectoryDone();
         } else {
