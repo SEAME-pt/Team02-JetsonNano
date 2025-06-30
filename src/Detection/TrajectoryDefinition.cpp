76f41245488c1b1a09ec994b30ab311ffabbd6b8
ruipires <ruipedro.pires999@gmail.com>
2025-06-30 08:53:46 +0100

    fix: update kalmanFilter predictions to use frame dimensions

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -240,8 +240,8 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
     }
     else
     {
-        leftCurve  = kalmanFilter->predictLeftLaneCurve();
-        rightCurve = kalmanFilter->predictRightLaneCurve();
+        leftCurve  = kalmanFilter->predictLeftLaneCurve(frameHeight_, frameWidth_);
+        rightCurve = kalmanFilter->predictRightLaneCurve(frameHeight_, frameWidth_);
     }
 
     defineTrajectoryCurve(midCurve, leftCurve, rightCurve);
