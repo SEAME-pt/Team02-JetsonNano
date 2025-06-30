13a72dd49c09134105d17385883f805cc3b14446
ruipires <ruipedro.pirees999@gmail.com>
2025-06-30 11:26:04 +0100

    fix: update lane clustering parameter in createLanes for improved lane detection

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -212,7 +212,7 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
     frameWidth_      = frame.cols;
     frameHeight_     = frame.rows;
 
-    lanePolylines = clusterLaneMask(binary_mask, frameWidth_ * frameHeight_ / 6000, frameWidth_ * frameHeight_ / 3000, 6);
+    lanePolylines = clusterLaneMask(binary_mask, frameWidth_ * frameHeight_ / 6000, frameWidth_ * frameHeight_ / 2000, 6);
     
     drawPolyLanes(lanePolylines);
     // float maxHorizontalDistance = frameWidth_ * 0.10;  // 15% of frame width
