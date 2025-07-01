4188ebcb393163653e375f43fd271304601ad772
masoares <mjsmarquescruz@gmail.com>
2025-07-01 17:06:39 +0100

    fix: adjust PID controller parameters for improved performance in local mode

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -52,10 +52,10 @@ int main(int argc, char** argv)
         if (mode == "local") {
             std::cout << "Running in LOCAL mode" << std::endl;
             // PID controller values
-            float kp                = 130;
+            float kp                = 110;
             float ki                = 0.000001;
             float kd                = 10;
-            float constant_speed    = 0.55;
+            float constant_speed    = 0.45;
             float delta_time        = 0.05;
 
             int screen_height = 480;
