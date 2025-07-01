b267268dac898510d62d512d3fb993270c0f3550
masoares <mjsmarquescruz@gmail.com>
2025-07-01 17:47:18 +0100

    fix: update alpha calculation in speedPID for improved gain scheduling

--- src/Controllers/SpeedPidController.cpp
+++ src/Controllers/SpeedPidController.cpp
@@ -169,8 +169,9 @@ float SpeedPidController::speedPID(float error, double current_time)
 
     // PID
     // Gain-scheduling index from steering (-1..1 -> 0..1)
-    // float alpha = std::min(1.0f, std::fabs(steer_));
-    float alpha = 1;
+    float alpha = std::min(1.0f, std::fabs(steer_));
+    // float alpha = std::max(0.3f, aplha);
+    // float alpha = 1;
 
     // Interpolate process model parameters
     float Kp_model = (1.0f - alpha) * Kp_s_ + alpha * Kp_c_;
