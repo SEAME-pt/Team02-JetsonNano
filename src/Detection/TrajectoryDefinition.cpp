0931d529913e77b2c8d6f563dfa5a7a6958f88ad
ruipires <ruipedro.pirees999@gmail.com>
2025-07-01 18:41:26 +0100

    fix: adjust min and max distance calculations in checkPredicedCurve for improved lane width accuracy

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -778,8 +778,8 @@ void TrajectoryDefinition::checkPredicedCurve(
     const std::vector<cv::Point>& realLane, bool isLeftLane)
 {
     float expectedWidth = calculateHistoricalLaneWidth();
-    float minDistance = calculateHistoricalLaneWidth() * 0.80;
-    float maxDistance = calculateHistoricalLaneWidth() * 1.20;
+    float minDistance = calculateHistoricalLaneWidth() * 0.85;
+    float maxDistance = calculateHistoricalLaneWidth() * 1.15;
 
     float realDistance = calculateLaneDistance(realLane, predictedCurve);
 
