31c41b64efa19de87866dd8f04fe1aa18f4f03f5
masoares <mjsmarquescruz@gmail.com>
2025-07-02 16:23:40 +0100

    refactor: update max_integral_ calculation to enhance integral term clamping in speedPID function

--- src/Controllers/SpeedPidController.cpp
+++ src/Controllers/SpeedPidController.cpp
@@ -195,7 +195,7 @@ float SpeedPidController::speedPID(float error, double current_time)
     // PID terms with anti-windup
     float p_term = kp_ * error;
     integral_  += error * dt;
-    max_integral_ = 5 / ki_; 
+    max_integral_ = 10 / ki_; 
     // Limit integral term to prevent windup
     integral_   = std::clamp(integral_, -max_integral_, max_integral_);
     float i_term = ki_ * integral_;
@@ -209,6 +209,8 @@ float SpeedPidController::speedPID(float error, double current_time)
     std::cout << "dt : " << dt << " | P:" << p_term << " I:" << i_term << " D:" << d_term 
               << " FF:" << u_ff << " | Integral:" << integral_ 
               << " | Output:" << throttle << std::endl;
+
+    last_time_ = current_time;
     return throttle;
 }
 
