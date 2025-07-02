af7886f0b946918e70c71b4d60db6c0ec720413f
masoares <mjsmarquescruz@gmail.com>
2025-07-02 12:17:59 +0100

    refactor: uncomment getCurrentTime function for proper time retrieval

--- src/Controllers/SpeedPidController.cpp
+++ src/Controllers/SpeedPidController.cpp
@@ -1,11 +1,11 @@
 #include "SpeedPidController.hpp"
 
-// static double getCurrentTime()
-// {
-//     struct timeval tv;
-//     gettimeofday(&tv, NULL);
-//     return tv.tv_sec + tv.tv_usec * 1e-6;
-// }
+static double getCurrentTime()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
+}
 
 
 SpeedPidController::SpeedPidController(std::shared_ptr<zenoh::Session> session, XboxController* xbox_controller)
