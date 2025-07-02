f1ef58dd0a4a4f3f8721cf3254ad49df9ccc66b2
ruipires <ruipedro.pirees999@gmail.com>
2025-07-02 11:09:23 +0100

    fix: add debug output for frame data in preProcess function

--- src/Detection/LaneDetector.cpp
+++ src/Detection/LaneDetector.cpp
@@ -47,6 +47,7 @@ void LaneDetector::preProcess(cv::Mat& frame, cv::Mat& preprocessedFrame)
 {
     // cv::Mat resized;
     
+    std::cout << frame.at<cv::Vec3b>(0,0) << std::endl;
     cv::resize(frame, preprocessedFrame, cv::Size(width_, height_), 0, 0, cv::INTER_LINEAR);
     
     // cv::cvtColor(resized, preprocessedFrame, cv::COLOR_BGR2RGB);
