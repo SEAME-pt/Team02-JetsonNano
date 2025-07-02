2b6ed9c7c2d412896d386af369bfac410b8b79ca
masoares <mjsmarquescruz@gmail.com>
2025-07-02 15:28:32 +0100

    refactor: adjust Kp_c value for improved cornering performance

--- include/Controllers/SpeedPidController.hpp
+++ include/Controllers/SpeedPidController.hpp
@@ -48,7 +48,7 @@ private:
     float tau_s_ = 0.5;
     float L_s_ = 0.25;
     // Cornering FOPDT model
-    float Kp_c_ = 300;
+    float Kp_c_ = 142;
     float tau_c_ = 0.5574;
     float L_c_ = 0.1750;
 
