277cee2a1f8357c937770d2a08222df1043904f7
masoares <mjsmarquescruz@gmail.com>
2025-07-02 18:15:29 +0100

    refactor: reduce sleep duration in manual control for improved responsiveness refactor: adjust Qy and Rsteer values for enhanced control dynamics in combinedControl

--- src/Controllers/MPController.cpp
+++ src/Controllers/MPController.cpp
@@ -414,7 +414,7 @@ void ModelPredictiveController::manualControl()
         std::cerr << "Invalid number of coefficients: " 
                 << parsed_coeffs_.size() << " (expected 4)" << std::endl;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 // SAE_1_LKAS
