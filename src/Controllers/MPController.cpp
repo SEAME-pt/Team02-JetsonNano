de8d5fdf2629b6c0ada6a7a9cd950afa5faf2e1f
masoares <mjsmarquescruz@gmail.com>
2025-07-03 11:14:23 +0100

    refactor: update reference velocity calculation in solve function for consistency

--- src/Controllers/MPController.cpp
+++ src/Controllers/MPController.cpp
@@ -141,7 +141,8 @@ void ModelPredictiveController::solve(const Eigen::Vector4d& x0,
     std::vector<Eigen::Vector4d> x_ref(N_ + 1);
     const double v_init = x0(3);
     for (size_t k = 0; k <= N_; ++k) {
-        double v_ref = v_init + (target_velocity_ - v_init) * double(k) / N_;
+        // double v_ref = v_init + (target_velocity_ - v_init) * double(k) / N_;
+        double v_ref = target_velocity_;
         double y_ref = x0(1) + k * v_ref * Ts_;
         double x_ref_m = meter_coeffs[0]
                        + meter_coeffs[1] * y_ref
