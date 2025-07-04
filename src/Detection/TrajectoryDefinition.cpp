dcefcaba271aa274bee674beb8303c8a1aa21889
ruipires <ruipedro.pires999@gmail.com>
2025-07-04 12:27:18 +0100

    fix: enable curve straightness check for leftCoeffs in checkAutomationLevel method

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -1158,11 +1158,10 @@ void TrajectoryDefinition::checkAutomationLevel(std::vector<cv::Point>& leftCurv
     //     return ;
     // }
 
-    (void) leftCoeffs;
     !isCurveStraight(rightCoeffs, 1e-3);
     std::cout << "Right" << std::endl;
-    // !isCurveStraight(leftCoeffs, 1e-3);
-    // std::cout << "Left" << std::endl;
+    !isCurveStraight(leftCoeffs, 1e-3);
+    std::cout << "Left" << std::endl;
 
     float centerX  = frameWidth_ / 2;
     float laneWidth = calculateHistoricalLaneWidth();
