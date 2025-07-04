2b9b22eab0a78e2b78a4100dd00d96ea193f6d66
ruipires <ruipedro.pirees999@gmail.com>
2025-07-04 11:47:07 +0100

    fix: update defineLaneEnv and filterFalseLanes method signatures for consistency

--- include/Detection/TrajectoryDefinition.hpp
+++ include/Detection/TrajectoryDefinition.hpp
@@ -98,13 +98,13 @@ class TrajectoryDefinition
     void createLanes(cv::Mat& frame, cv::Mat& binary_mask, cv::Mat& class_mask);
     std::vector<std::vector<cv::Point>> clusterLaneMask(const cv::Mat& laneMask, int kernelSize, int minArea, int maxLanes);
     void mergeLaneComponents(std::vector<std::vector<cv::Point>>& lanePolylines, float maxHorizontalDist, float maxVerticalGap);
-    void defineLaneEnv(std::vector<std::vector<cv::Point>> &lanePolylines, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve, cv::Mat& leftCoeffs, cv::Mat& rightCoeffs);
+    void defineLaneEnv(std::vector<std::vector<cv::Point>> &lanePolylines, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve, std::vector<cv::Mat> &coeffsSave);
     void onePolyline(std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve);
     void twoPolylines(std::vector<std::vector<cv::Point>> lanePolylines, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve);
     void drawPolyLanes(std::vector<std::vector<cv::Point>> lanePolylines);
     bool checkIfLeftLane(const std::vector<cv::Point>& lanePolyline);
 
-    void filterFalseLanes(std::vector<std::vector<cv::Point>> &lanePolylines, std::vector<cv::Mat>> &coeffsSave);
+    void filterFalseLanes(std::vector<std::vector<cv::Point>> &lanePolylines, std::vector<cv::Mat> &coeffsSave);
     void lowerPointLaneDefinition(std::vector<std::vector<cv::Point>> &lanePolylines, std::vector<cv::Point>& leftCurve, std::vector<cv::Point>& rightCurve);
 
     float calculateHistoricalLaneWidth();
