10d1491cfd14552a6f938e61a2625d5f810d3bd4
ruipires <ruipedro.pirees999@gmail.com>
2025-07-11 19:17:40 +0100

    fix: increase constant speed parameter for improved performance

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -55,7 +55,7 @@ int main(int argc, char** argv)
             float kp                = 100;
             float ki                = 0.000001;
             float kd                = 10;
-            float constant_speed    = 0.25;
+            float constant_speed    = 0.35;
             float delta_time        = 0.05;
 
             int screen_height = 480;
