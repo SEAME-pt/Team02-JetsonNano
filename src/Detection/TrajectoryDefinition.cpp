894118eec5fff93b6002dc44bd6732e5c81559eb
ruipires <ruipedro.pires999@gmail.com>
2025-07-11 17:16:41 +0100

    fix: adjust near and far distance parameters in initLocalEnv method for improved accuracy

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -122,8 +122,8 @@ void TrajectoryDefinition::initLocalEnv() {
         float img_width = static_cast<float>(width_);
         float h_fov_rad = horizontalFOV * CV_PI / 180.0f;
         float verticalFOV = 2.0f * std::atan((img_height/img_width) * std::tan(h_fov_rad/2.0f)) * 180.0f / CV_PI;
-        nearDistance_ = 0.3f;       // meters
-        farDistance_ = 1.2f;       // meters
+        nearDistance_ = 0.2f;       // meters
+        farDistance_ = 0.8f;       // meters
         laneWidth_ = 0.6f;      // meters
         cv::Size bevSize = cv::Size(width_, height_);
         cv::Size origSize = cv::Size(width_, height_);
