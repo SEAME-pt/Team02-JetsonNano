e0de36ecf0270c79ed0cb7a52f8fa88c2da24b1f
ruipires <ruipedro.pires999@gmail.com>
2025-07-14 13:36:35 +0100

    fix: restore collision check and comment out obstacle avoidance for SAE_4

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -281,15 +281,15 @@ void TrajectoryDefinition::createLanes(cv::Mat& frame, cv::Mat& binary_mask,
 
     drawCurves(midCurve, leftCurve, rightCurve);
 
-    // if (activeAutonomyLevel_ != "SAE_0") {
-    //     checkForwardCollision(class_mask, midCurve);
-    // } 
+    if (activeAutonomyLevel_ != "SAE_0") {
+        checkForwardCollision(class_mask, midCurve);
+    } 
     
     if (activeAutonomyLevel_ == "SAE_2" || 
         activeAutonomyLevel_ == "SAE_3" ||
         activeAutonomyLevel_ == "SAE_4") {
-        if(activeAutonomyLevel_ == "SAE_4")
-            obstacleAvoidance(class_mask, midCurve);
+        // if(activeAutonomyLevel_ == "SAE_4")
+        //     obstacleAvoidance(class_mask, midCurve);
         createMidPointError(midCurve);
         publishCoeffs(midCurve);
     }
