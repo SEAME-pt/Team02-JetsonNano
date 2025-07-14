ddbaf02b8a478c5d08345e33659b9709a5624bac
ruipires <ruipedro.pirees999@gmail.com>
2025-07-14 15:26:05 +0100

    fix: correct offset direction in checkPredicedCurve for accurate lane prediction

--- src/Detection/TrajectoryDefinition.cpp
+++ src/Detection/TrajectoryDefinition.cpp
@@ -927,8 +927,8 @@ void TrajectoryDefinition::checkPredicedCurve(
                 cv::Point2f normal(-dir.y, dir.x);
 
                 // Offset along normal
-                float offset = -expectedWidth;
-                cv::Point2f newPt = pt - normal * offset;
+                float offset = expectedWidth;
+                cv::Point2f newPt = pt + normal * offset;
 
                 predictedCurve.push_back(cv::Point(static_cast<int>(newPt.x), static_cast<int>(newPt.y)));
             }
