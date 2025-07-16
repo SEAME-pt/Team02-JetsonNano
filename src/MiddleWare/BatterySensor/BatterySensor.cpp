f3661a7237f31df9ed973383dd9da31c27395cb1
masoares <mjsmarquescruz@gmail.com>
2025-07-17 00:38:38 +0100

    fix: comment out CAN device existence check in initLocalEnv method

--- src/MiddleWare/BatterySensor/BatterySensor.cpp
+++ src/MiddleWare/BatterySensor/BatterySensor.cpp
@@ -48,12 +48,12 @@ void BatterySensor::initLocalEnv(const std::string& i2cDevice, uint8_t sensorAdd
     }
 
     try {
-        struct stat buffer;
-        if (stat(canDevice.c_str(), &buffer) != 0) {
-            std::cerr << "Can device " << canDevice << " does not exist!" << std::endl;
-            this->canBus = NULL;
-            throw std::runtime_error("Error on can device");
-        }
+        // struct stat buffer;
+        // if (stat(canDevice.c_str(), &buffer) != 0) {
+        //     std::cerr << "Can device " << canDevice << " does not exist!" << std::endl;
+        //     this->canBus = NULL;
+        //     throw std::runtime_error("Error on can device");
+        // }
         this->canBus     = new CAN();
         this->canBus->init(canDevice);
     } catch (...) {
