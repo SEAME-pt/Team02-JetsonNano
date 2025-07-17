2b8d546ec6eb1fb120479d799a466254848ae62d
masoares <mjsmarquescruz@gmail.com>
2025-07-17 15:10:04 +0100

    fix: adjust Kp_s_ and Kp_c_ parameters for improved PID control

--- include/Controllers/SpeedPidController.hpp
+++ include/Controllers/SpeedPidController.hpp
@@ -44,13 +44,13 @@ private:
 
     //calibration parameters
     // Straight-line FOPDT model
-    float Kp_s_= 3;
+    float Kp_s_= 1;
     float tau_s_ = 0.50;
-    float L_s_ = 0.00;
+    float L_s_ = 0.5;
     // Cornering FOPDT model
-    float Kp_c_ = 3.5;
+    float Kp_c_ = 1.5;
     float tau_c_ = 0.33;
-    float L_c_ = 0.20;
+    float L_c_ = 0.5;
 
     float a0_ = 20.0f;
     // float a0_ = 14.93f;
