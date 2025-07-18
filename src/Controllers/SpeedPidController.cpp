8242a1468abe4cac5a20401a054e27d3234515f5
masoares <mjsmarquescruz@gmail.com>
2025-07-18 15:07:49 +0100

    refactor: comment out debug output for alpha in speedPID function

--- src/Controllers/SpeedPidController.cpp
+++ src/Controllers/SpeedPidController.cpp
@@ -185,7 +185,7 @@ float SpeedPidController::speedPID(float error, double current_time)
     // integral_   = std::clamp(integral_, -max_integral_, max_integral_);
     float i_term = ki_ * integral_;
     float d_term = kd_ * ((error - prev_error_) / dt);
-    std::cout << "ALPHA : " << alpha << std::endl;
+    // std::cout << "ALPHA : " << alpha << std::endl;
     std::cout << "kp : " << kp_ << " | ki : " << ki_ << " | kd : " << kd_ << std::endl;
     std::cout << "p_term : " << p_term << " | i_term : " << i_term 
               << " | d_term : " << d_term << std::endl;
