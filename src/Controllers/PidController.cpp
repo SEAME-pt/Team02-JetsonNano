71408a1e8f985314559cf004f9d52fea4db322f1
ruipires <ruipedro.pires999@gmail.com>
2025-07-21 17:40:22 +0100

    refactor: update active_speed initialization in speedDefinition method to use speed_limit_

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -364,7 +364,7 @@ void PidController::speedDefinition(void) {
     double current_time = getCurrentTime();
     double threshold = 0.80;
     double red_threshold = 5.0;
-    double active_speed = 0.0f;
+    double active_speed = speed_limit_;
 
     if (std::abs(current_time -  last_acc_speed_receive_) < threshold) {
         if (acc_speed_ > speed_limit_)
