58bbb5cf759bfab557b3ff9caa81b8347d287456
mjorgecruz <mjsmarquescruz@gmail.com>
2025-08-08 11:15:53 +0100

    fix: simplify speed publishing by removing unnecessary memory allocation

--- src/Communication/SensoringPublisher.cpp
+++ src/Communication/SensoringPublisher.cpp
@@ -24,12 +24,12 @@ SensoringPublisher::SensoringPublisher(std::shared_ptr<zenoh::Session> session)
 void SensoringPublisher::publishSpeed(float speed)
 {
     std::string value_str = std::to_string(speed);
-    const auto len        = value_str.size() + 1;
-    auto alloc_result =
-        provider_->alloc_gc_defrag_blocking(len, zenoh::AllocAlignment({0}));
-    zenoh::ZShmMut&& buf = std::get<zenoh::ZShmMut>(std::move(alloc_result));
-    memcpy(buf.data(), value_str.c_str(), len);
-    speed_pub->put(std::move(buf));
+    // const auto len        = value_str.size() + 1;
+    // auto alloc_result =
+    //     provider_->alloc_gc_defrag_blocking(len, zenoh::AllocAlignment({0}));
+    // zenoh::ZShmMut&& buf = std::get<zenoh::ZShmMut>(std::move(alloc_result));
+    // std::memcpy(speed, value_str.c_str(), len);
+    speed_pub->put(value_str);
 }
 
 void SensoringPublisher::publishCurrentVoltage(float voltage)
