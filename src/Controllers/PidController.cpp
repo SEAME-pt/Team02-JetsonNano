a0eda7b91442f75168cd4ef349c4eab3a7d6cda6
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-08-14 10:23:06 +0100

    fix: increase sleep duration in run method for improved timing control

--- src/Controllers/PidController.cpp
+++ src/Controllers/PidController.cpp
@@ -511,7 +511,7 @@ void PidController::run()
         }
         else
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
         }
     }
 }
