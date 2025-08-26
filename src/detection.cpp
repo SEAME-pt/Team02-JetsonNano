979f79c8616217ce9b295cfb0717333d5391d9e9
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-08-26 16:11:05 +0100

    Reduce video writer frame rate in trajectoryThreadFunction to improve performance

--- src/detection.cpp
+++ src/detection.cpp
@@ -97,8 +97,8 @@ void trajectoryThreadFunction(TrajectoryDefinition* trajectoryDef,
                 trajectoryDef->process(original_frame, lane_mask, object_mask);
 
             if (!writersInitialized) {
-                ipmWriter.open("ipm_output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 100, new_frame.size());
-                origWriter.open("orig_output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 100, original_frame.size());
+                ipmWriter.open("ipm_output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 20, new_frame.size());
+                origWriter.open("orig_output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 20, original_frame.size());
                 // laneWriter.open("lane_output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 100, lane_mask.size());
                 // objWriter.open("obj_output.avi", cv::VideoWriter::fourcc('M','J','P','G'), 100, object_mask.size());
                 writersInitialized = true;
