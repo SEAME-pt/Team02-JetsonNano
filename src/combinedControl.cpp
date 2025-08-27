24754094129625623d351f43e9c7448df02adc74
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-08-27 15:01:09 +0100

    Update constant_speed in main function from 0.30 to 0.35 m/s for improved performance

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -71,7 +71,7 @@ int main(int argc, char** argv)
             float kp             = 130;
             float ki             = 0.000001;
             float kd             = 10;
-            float constant_speed = 0.30; // m/s
+            float constant_speed = 0.35; // m/s
             float delta_time     = 0.05;
 
             int screen_height = 480;
