ce151a232c0a1a210033d64cca7e799c723c0976
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-08-29 15:10:36 +0100

    Update trajectory thread function to change output folder name to "output_lkas"

--- src/detection.cpp
+++ src/detection.cpp
@@ -86,7 +86,7 @@ void trajectoryThreadFunction(TrajectoryDefinition* trajectoryDef,
 {
     cv::Mat original_frame, lane_mask, object_mask;
 
-    std::string save_folder = "output_emergencyBrake";
+    std::string save_folder = "output_lkas";
     mkdir(save_folder.c_str(), 0777); 
 
     while (running)
