5e29403b157219d9753870da965e21aa5001ee60
mjorgecruz <mjsmarquescruz@gmail.com>
2025-08-29 17:48:06 +0100

    Update Qx and Rthrottle parameters for improved control performance

--- src/combinedControl.cpp
+++ src/combinedControl.cpp
@@ -16,8 +16,8 @@ int main(int argc, char** argv)
             return -1;
         }
 
-        double Qx = 3000.0, Qy = 400.0, Qpsi = 300.0, Qv = 250.0,
-               Rthrottle = 100.0, Rsteer = 40.0;
+        double Qx = 2400.0, Qy = 600.0, Qpsi = 300.0, Qv = 250.0,
+               Rthrottle = 20.0, Rsteer = 40.0;
         for (int i = 1; i < argc; ++i)
         {
             if (std::string(argv[i]) == "--Qx" && i + 1 < argc)
