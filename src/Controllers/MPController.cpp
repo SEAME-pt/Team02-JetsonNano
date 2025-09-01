e4cd001bdd2b81f886f3d2a0217b4896c6d24947
mjorgecruz <mjsmarquescruz@gmail.com>
2025-09-01 09:58:20 +0100

    Update max_allowed_speed calculation and ensure desired_speed is a float in solve function

--- src/Controllers/MPController.cpp
+++ src/Controllers/MPController.cpp
@@ -358,8 +358,8 @@ void ModelPredictiveController::solve(const Eigen::Vector4d& x0,
     // set outputs
     // if (carlaMode_) {
     desired_speed_ = u_flat(0);
-    const double max_allowed_speed = target_velocity_ * 1.5;
-    desired_speed_ = std::clamp(desired_speed_, 0.0, max_allowed_speed);
+    double max_allowed_speed = target_velocity_ * 1.5;
+    desired_speed_ = static_cast<float>(std::clamp(desired_speed_, 0.0, max_allowed_speed));
     // } else {
     // desired_speed_ = u_flat(0) * 60.0 / (M_PI * 0.067); // convert from m/s
     // to rpm
